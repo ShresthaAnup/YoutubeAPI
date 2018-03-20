80741c78edade963b6f1a1d4cbca297e63d132f2
Anup Shrestha <Anup.Shrestha@infokonn.com>
2018-03-20 15:52:20 +0545

    added search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function(){
-	return <div>Hello World</div>
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCVxZ6MOb5D07T7WVoQnyWpbhBda79dKxg'
+
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	)
 }
 
 ReactDOM.render(
