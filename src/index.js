55e9f64aa3b36512d84353ae973533fca9ff5979
Anup Shrestha <Anup.Shrestha@infokonn.com>
2018-03-21 12:45:58 +0545

    added video list and details

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCVxZ6MOb5D07T7WVoQnyWpbhBda79dKxg'
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	)
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { 
+			videos: [] ,
+			selectedVideo: null
+		};
+
+		this.videoSearch('hello world');
+
+		
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, videos => {
+				this.setState({ 
+					videos: videos,
+					selectedVideo: videos[0]
+				});
+				//this.setState({ videos: videos})
+		});
+	}
+
+	render (){
+		const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+		return (
+			<div>
+				<SearchBar 
+					onSearchTermChange = {videoSearch}
+					/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect= { selectedVideo => this.setState({selectedVideo}) }
+					videos={ this.state.videos } 
+					/>
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(
